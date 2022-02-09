714744fbd8cdd0149551f485eb6bb7ddf95d774d
Vincent Orback <hello@vincentorback.se>
2022-02-09 10:07:48 +0100

    test some experimental next settings

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
+const isProduction = process.env.NETLIFY
+
 const withTranslateRoutes = require('next-translate-routes/plugin')
 
 const nextConfig = withTranslateRoutes({
   generateBuildId: () => 'build',
   reactStrictMode: true,
+  swcMinify: true,
   i18n: {
     locales: ['sv', 'en'],
     defaultLocale: 'sv',
     localeDetection: true,
+  },
+  experimental: {
+    reactRemoveProperties: true,
+    removeConsole: isProduction,
   },
 })
 
