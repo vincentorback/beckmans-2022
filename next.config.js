cce3e775432bac773344be6cfeb55d241bbbf2b1
Vincent Orback <hello@vincentorback.se>
2022-02-09 10:11:30 +0100

    guess not

--- next.config.js
+++ next.config.js
@@ -5,15 +5,10 @@ const withTranslateRoutes = require('next-translate-routes/plugin')
 const nextConfig = withTranslateRoutes({
   generateBuildId: () => 'build',
   reactStrictMode: true,
-  swcMinify: true,
   i18n: {
     locales: ['sv', 'en'],
     defaultLocale: 'sv',
     localeDetection: true,
-  },
-  experimental: {
-    reactRemoveProperties: true,
-    removeConsole: isProduction,
   },
 })
 
