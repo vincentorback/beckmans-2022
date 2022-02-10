1d2d1bd50f7776529308a20196d5de515997ea53
Vincent Orback <hello@vincentorback.se>
2022-02-10 14:20:06 +0100

    testing framer-motion

--- components/Filters/index.js
+++ components/Filters/index.js
@@ -2,21 +2,46 @@ import classNames from 'classnames'
 import styles from './filters.module.css'
 import { slugify } from '../../lib/utilities'
 import { useTranslations } from 'next-intl'
+import { motion } from 'framer-motion'
 
-const Filters = ({ filters, activeFilter, onClick }) => {
+const Filters = ({ isReady, filters, activeFilter, onClick }) => {
   const t = useTranslations()
 
   return (
     <div className={styles.filters}>
-      {filters.map((filter) => (
+      {filters.map((filter, filterIndex) => (
         <button
+          aria-pressed={activeFilter === filter}
           key={filter}
           onClick={() => onClick(filter)}
           className={classNames(styles.button, {
-            [styles['is-active']]: !activeFilter || activeFilter === filter,
+            [styles['is-active']]: activeFilter === filter,
           })}
         >
-          {t(`categories.${slugify(filter)}`)}
+          <motion.div
+            variants={{
+              hidden: {
+                opacity: 0,
+                y: '50%',
+              },
+              enter: {
+                opacity: 1,
+                y: 0,
+              },
+              exit: {
+                opacity: 0,
+                y: '-50%',
+              },
+            }}
+            initial={{
+              opacity: 0,
+            }}
+            animate={isReady ? 'enter' : 'hidden'}
+            exit="exit"
+            transition={{ type: 'ease', delay: 0.5 + 0.1 * filterIndex }}
+          >
+            {t(`categories.${slugify(filter)}`)}
+          </motion.div>
         </button>
       ))}
     </div>
