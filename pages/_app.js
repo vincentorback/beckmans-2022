27c63a7a4442e4e2aa0c6d9dc5654ccb0c46a79f
Vincent Orback <hello@vincentorback.se>
2022-02-12 18:33:12 +0100

    bump packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,11 @@
-import React from 'react'
 import { NextIntlProvider } from 'next-intl'
 import withTranslateRoutes from 'next-translate-routes'
-import dynamic from 'next/dynamic'
 import '../styles/index.css'
 
-// const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
-//   ssr: false,
-// })
-
-const App = ({ Component, pageProps, router }) => {
-  return (
-    <>
-      <NextIntlProvider messages={pageProps.messages}>
-        <Component {...pageProps} key={router.route} />
-      </NextIntlProvider>
-      {/* <AnimatedCursor
-        // innerSize={12}
-        // outerSize={12}
-        color="221, 117, 80"
-        // outerAlpha={0.1}
-        // innerScale={0.5}
-        // outerScale={3}
-      /> */}
-    </>
-  )
-}
+const App = ({ Component, pageProps, router }) => (
+  <NextIntlProvider messages={pageProps.messages}>
+    <Component {...pageProps} key={router.route} />
+  </NextIntlProvider>
+)
 
 export default withTranslateRoutes(App)
