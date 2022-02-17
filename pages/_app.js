2741926dc4f24a1f6426c3f7cb406a8779e4cf86
Vincent Orback <hello@vincentorback.se>
2022-02-17 14:48:19 +0100

    begin work on intro

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,23 @@ import React from 'react'
 import { LazyMotion, domAnimation, MotionConfig } from 'framer-motion'
 import { NextIntlProvider } from 'next-intl'
 import withTranslateRoutes from 'next-translate-routes'
+import Intro from '../components/Intro'
 import '../styles/index.css'
 
 const App = ({ Component, pageProps, router }) => {
+  // const [isFirstVisit, setIsFirstVisit] = React.useState(false)
+
+  // React.useEffect(() => {
+  //   if (!localStorage.firstVisit) {
+  //     setIsFirstVisit(true)
+  //     localStorage.firstVisit = false
+  //   }
+  // }, [])
+
+  // const handleCloseIntro = React.useCallback(() => {
+  //   setIsFirstVisit(false)
+  // }, [])
+
   return (
     <NextIntlProvider messages={pageProps.messages}>
       <LazyMotion features={domAnimation} strict>
@@ -12,6 +26,7 @@ const App = ({ Component, pageProps, router }) => {
           <Component {...pageProps} key={router.route} />
         </MotionConfig>
       </LazyMotion>
+      {/* {isFirstVisit && <Intro closeIntro={handleCloseIntro} />} */}
     </NextIntlProvider>
   )
 }
