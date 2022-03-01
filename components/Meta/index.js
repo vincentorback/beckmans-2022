d3ab1cc09f4043927df41ce957c007b86d5acbf2
Vincent Orback <hello@vincentorback.se>
2022-03-01 11:38:58 +0100

    add dates to page title

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -8,15 +8,13 @@ const Meta = ({ title, children, otherLocalePage }) => {
   const router = useRouter()
   const otherLocale = router.locales.find((item) => item !== router.locale)
 
-  console.log(router)
-
   return (
     <Head>
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
       <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <title>
-        {title && `${title} - `}Beckmans {t('show')} 2022
+        {title && `${title} - `}Beckmans {t('show')} 19.05–24.05.2022
       </title>
       {/* TODO: <meta name="description" content="" />
       <meta property="og:image" content="" />
