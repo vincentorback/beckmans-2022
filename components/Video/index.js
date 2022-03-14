5d64d368176d12fe958efbfd7f0da591e0d288c5
Vincent Orback <hello@vincentorback.se>
2022-03-14 13:59:51 +0100

    catch global video errors

--- components/Video/index.js
+++ components/Video/index.js
@@ -8,9 +8,35 @@ import 'plyr-react/dist/plyr.css'
 import styles from './video.module.css'
 
 const Video = ({ id, provider, width, height, html }) => {
+  const videoRef = React.useRef()
+
   const { ref, inView } = useInView({
     triggerOnce: true,
   })
+
+  React.useEffect(() => {
+    const plyr = videoRef?.current?.plyr
+
+    const onError = (error) => {
+      console.log(error)
+      // const errorMessage = e?.reason?.message || error?.message
+    }
+
+    if (plyr?.on) {
+      plyr.on('error', onError)
+    }
+
+    window.addEventListener('error', onError)
+    window.addEventListener('unhandledrejection', onError)
+
+    return () => {
+      window.removeEventListener('error', onError)
+      window.removeEventListener('unhandledrejection', onError)
+      if (plyr?.off) {
+        plyr.off('error', onError)
+      }
+    }
+  }, [inView])
 
   // TODO: On error, replace with {html}
 
@@ -26,6 +52,7 @@ const Video = ({ id, provider, width, height, html }) => {
         }}
       >
         <Plyr
+          ref={videoRef}
           source={{
             type: 'video',
             sources: [
