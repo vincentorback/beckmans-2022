f118528e03381a25d6dd9e1f99b37495abc962da
Vincent Orback <hello@vincentorback.se>
2022-03-18 10:58:25 +0100

    test ugly css-modules persist function

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,12 @@ import {
 } from 'framer-motion'
 import { NextIntlProvider } from 'next-intl'
 import withTranslateRoutes from 'next-translate-routes'
+import { fixTimeoutTransition } from '../lib/utilities'
 import '../styles/index.css'
 
 const App = ({ Component, pageProps, router }) => {
+  fixTimeoutTransition(1000)
+
   return (
     <NextIntlProvider messages={pageProps.messages}>
       <LazyMotion features={domAnimation} strict>
