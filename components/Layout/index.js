2e0c1619e5b92f1f06998a4133ae597244541613
Vincent Orback <hello@vincentorback.se>
2022-03-24 16:14:05 +0100

    add preview button

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Meta from '../Meta'
 import Header from '../Header'
 import Footer from '../Footer'
+import Link from 'next-translate-routes/link'
+import { useRouter } from 'next/router'
 
 const Layout = ({ title, children, background, pages, otherLocalePage }) => {
+  const router = useRouter()
   const backgroundColor = background ? background.toLowerCase() : 'white'
 
   return (
@@ -13,6 +16,11 @@ const Layout = ({ title, children, background, pages, otherLocalePage }) => {
       </Meta>
       <div className="Layout-main">{children}</div>
       <Footer pages={pages} otherLocalePage={otherLocalePage} />
+      {router.isPreview && (
+        <Link href={'/api/exit-preview'} prefetch={false}>
+          <a className={'Layout-previewButton'}>Exit preview</a>
+        </Link>
+      )}
     </div>
   )
 }
