f857d9aa97676a520b95327634608ddbc76ffc9f
Vincent Orback <hello@vincentorback.se>
2022-04-19 20:25:25 +0200

    add background behind credit columns

--- components/Credits/index.js
+++ components/Credits/index.js
@@ -7,14 +7,16 @@ const Credits = ({ columns }) => {
       <div className="Credits-columns">
         {columns.map((column, columnIndex) => (
           <div className="Credits-column" key={`column_${columnIndex}`}>
-            {column.column_title && (
-              <h2 className="Credits-title">{column.column_title}</h2>
-            )}
-            {column.column_text && (
-              <Entry>
-                <PrismicRichText field={column.column_text} />
-              </Entry>
-            )}
+            <div className="Credits-columnInner">
+              {column.column_title && (
+                <h2 className="Credits-title">{column.column_title}</h2>
+              )}
+              {column.column_text && (
+                <Entry>
+                  <PrismicRichText field={column.column_text} />
+                </Entry>
+              )}
+            </div>
           </div>
         ))}
       </div>
