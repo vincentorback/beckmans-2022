0d17b5b679a04ea56ac3db699c10e8fe05b2d549
Vincent Orback <hello@vincentorback.se>
2022-05-05 15:20:16 +0200

    optimize mini map performance

--- components/Map/index.js
+++ components/Map/index.js
@@ -31,142 +31,35 @@ const Map = ({ items, category }) => {
     setActiveItem(value)
   }, [])
 
-  const MemoMap = React.useMemo(() => {
-    return (
-      <div className="Map">
-        <p className="Map-activeTitle">{activeItem}</p>
-        <div className="Map-container">
-          <div className="Map-grid">
-            {items.map((item, itemIndex) => {
-              const isVisible =
-                item?.data?.category && item.data.category === category
+  const MemoDots = React.useMemo(() => <MapDots />, [])
 
-              const imageOriginalWidth =
-                item?.data?.main_image?.dimensions.width
-              const imageOriginalHeight =
-                item?.data?.main_image?.dimensions.height
+  const MemoTitle = React.useMemo(
+    () => <p className="Map-activeTitle">{activeItem}</p>,
+    [activeItem]
+  )
 
-              const rectImageWidth = 116
-              const rectImageHeight = 116
+  const MemoGrid = React.useMemo(
+    () => (
+      <MapGrid
+        handleMouse={handleMouse}
+        items={items}
+        category={category}
+        allImagesLoaded={allImagesLoaded}
+        handleImageLoad={handleImageLoad}
+      />
+    ),
+    [handleMouse, items, category, allImagesLoaded, handleImageLoad]
+  )
 
-              const imagePosition =
-                item?.data?.image_position &&
-                item.data.image_position.match('([0-9]{1,3}%) ([0-9]{1,3}%)')
-
-              const imagePositionX = imagePosition ? imagePosition[1] : '50%'
-              const imagePositionY = imagePosition ? imagePosition[2] : '50%'
-
-              const rectX =
-                imageOriginalWidth &&
-                Math.min(
-                  Math.floor(
-                    imageOriginalWidth * (imagePositionX.replace('%', '') / 100)
-                  ),
-                  imageOriginalWidth - rectImageWidth
-                )
-
-              const rectY =
-                imageOriginalHeight &&
-                Math.min(
-                  Math.floor(
-                    imageOriginalHeight *
-                      (imagePositionY.replace('%', '') / 100)
-                  ),
-                  imageOriginalHeight - rectImageHeight
-                )
-
-              return (
-                <m.div
-                  key={item.uid || itemIndex}
-                  className="Map-item"
-                  onMouseEnter={() =>
-                    isVisible && handleMouse(prismicH.asText(item.data.name))
-                  }
-                  onTouchStart={(e) => {
-                    if (isVisible) {
-                      e.preventDefault()
-                      handleMouse(prismicH.asText(item.data.name))
-                    }
-                  }}
-                  onMouseLeave={() => isVisible && handleMouse(null)}
-                  initial="initial"
-                  animate={
-                    allImagesLoaded && isVisible ? 'active' : 'notActive'
-                  }
-                  exit="exit"
-                  variants={{
-                    initial: {
-                      opacity: 0,
-                      scale: 0.75,
-                    },
-                    active: {
-                      opacity: 1,
-                      scale: 1,
-                      transition: {
-                        duration: 0.3,
-                        delay: 1 + itemIndex * 0.03,
-                      },
-                    },
-                    notActive: {
-                      opacity: 0,
-                      scale: 0.75,
-                      transition: {
-                        duration: 0.3,
-                        delay: itemIndex * 0.03,
-                      },
-                    },
-                    exit: {
-                      opacity: 0,
-                      scale: 0.75,
-                      transition: {
-                        duration: 0.2,
-                        delay: (isVisible ? itemIndex : 0) * 0.03,
-                      },
-                    },
-                  }}
-                >
-                  {isVisible && (
-                    <Link
-                      href={linkResolver(item)}
-                      prefetch={false}
-                      scroll={false}
-                    >
-                      <a className="Map-itemLink">
-                        <div className="Map-itemInner">
-                          <Image
-                            className="Map-itemImage"
-                            src={item.data.main_image}
-                            width={rectImageWidth}
-                            height={rectImageHeight}
-                            rect={`${rectX},${rectY},${rectImageWidth * 2},${
-                              rectImageHeight * 2
-                            }`}
-                            quality={10}
-                            onLoadingComplete={() => handleImageLoad(item.uid)}
-                            alt=""
-                          />
-                        </div>
-                      </a>
-                    </Link>
-                  )}
-                </m.div>
-              )
-            })}
-          </div>
-          <MapDots />
-        </div>
+  return (
+    <div className="Map">
+      {MemoTitle}
+      <div className="Map-container">
+        {MemoGrid}
+        {MemoDots}
       </div>
-    )
-  }, [
-    activeItem,
-    handleImageLoad,
-    handleMouse,
-    items,
-    category,
-    allImagesLoaded,
-  ])
-
-  return MemoMap
+    </div>
+  )
 }
 
 const MapDots = () => (
@@ -180,6 +73,122 @@ const MapDots = () => (
         }}
       />
     ))}
+  </div>
+)
+
+const MapGrid = ({
+  handleMouse,
+  items,
+  category,
+  allImagesLoaded,
+  handleImageLoad,
+}) => (
+  <div className="Map-grid">
+    {items.map((item, itemIndex) => {
+      const isVisible = item?.data?.category && item.data.category === category
+
+      const imageOriginalWidth = item?.data?.main_image?.dimensions.width
+      const imageOriginalHeight = item?.data?.main_image?.dimensions.height
+
+      const rectImageWidth = 116
+      const rectImageHeight = 116
+
+      const imagePosition =
+        item?.data?.image_position &&
+        item.data.image_position.match('([0-9]{1,3}%) ([0-9]{1,3}%)')
+
+      const imagePositionX = imagePosition ? imagePosition[1] : '50%'
+      const imagePositionY = imagePosition ? imagePosition[2] : '50%'
+
+      const rectX =
+        imageOriginalWidth &&
+        Math.min(
+          Math.floor(
+            imageOriginalWidth * (imagePositionX.replace('%', '') / 100)
+          ),
+          imageOriginalWidth - rectImageWidth
+        )
+
+      const rectY =
+        imageOriginalHeight &&
+        Math.min(
+          Math.floor(
+            imageOriginalHeight * (imagePositionY.replace('%', '') / 100)
+          ),
+          imageOriginalHeight - rectImageHeight
+        )
+
+      return (
+        <m.div
+          key={item.uid || itemIndex}
+          className="Map-item"
+          onMouseEnter={() =>
+            isVisible && !handleMouse(prismicH.asText(item.data.name))
+          }
+          onTouchStart={(e) => {
+            if (isVisible) {
+              e.preventDefault()
+              handleMouse(prismicH.asText(item.data.name))
+            }
+          }}
+          onMouseLeave={() => isVisible && handleMouse(null)}
+          initial="initial"
+          animate={allImagesLoaded && isVisible ? 'active' : 'notActive'}
+          exit="exit"
+          variants={{
+            initial: {
+              opacity: 0,
+              scale: 0.75,
+            },
+            active: {
+              opacity: 1,
+              scale: 1,
+              transition: {
+                duration: 0.3,
+                delay: 1 + itemIndex * 0.03,
+              },
+            },
+            notActive: {
+              opacity: 0,
+              scale: 0.75,
+              transition: {
+                duration: 0.3,
+                delay: itemIndex * 0.03,
+              },
+            },
+            exit: {
+              opacity: 0,
+              scale: 0.75,
+              transition: {
+                duration: 0.2,
+                delay: (isVisible ? itemIndex : 0) * 0.03,
+              },
+            },
+          }}
+        >
+          {isVisible && (
+            <Link href={linkResolver(item)} prefetch={false} scroll={false}>
+              <a className="Map-itemLink">
+                <div className="Map-itemInner">
+                  <Image
+                    className="Map-itemImage"
+                    src={item.data.main_image}
+                    width={rectImageWidth}
+                    height={rectImageHeight}
+                    rect={`${rectX},${rectY},${rectImageWidth * 2},${
+                      rectImageHeight * 2
+                    }`}
+                    quality={10}
+                    onLoadingComplete={() => handleImageLoad(item.uid)}
+                    alt=""
+                  />
+                </div>
+              </a>
+            </Link>
+          )}
+        </m.div>
+      )
+    })}
   </div>
 )
 
