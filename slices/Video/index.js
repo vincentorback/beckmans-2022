6c7bed4b6a0a49608a0776105e36d9a05191ad94
Vincent Orback <hello@vincentorback.se>
2025-09-18 21:44:27 +0200

    iframe embed

--- slices/Video/index.js
+++ slices/Video/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import classNames from 'classnames'
-import Plyr from 'plyr-react'
 import { useInView } from 'react-intersection-observer'
 import { getYoutubeID } from '../../lib/utilities'
 
@@ -8,103 +7,46 @@ const Video = ({ slice }) => {
   const { width, height, video_id, provider_name, html } =
     slice.primary.embedURL
 
-  const plyrRef = React.useRef()
-  const containerRef = React.useRef(null)
   const { ref, inView } = useInView({
     triggerOnce: true,
   })
-  const [hasError, setHasError] = React.useState(false)
 
-  React.useEffect(() => {
-    try {
-      if (plyrRef?.current?.plyr) {
-        const plyr = plyrRef?.current?.plyr
+  const provider = provider_name.toLowerCase()
 
-        if (plyr && plyr.source) {
-          const onError = () => {
-            if (html) {
-              containerRef.current.innerHTML = `<div class="Video-inner">${html}</div>`
-              setHasError(true)
-            }
-          }
+  const videoSrc =
+    video_id ??
+    (provider_name === 'youtube' && html ? getYoutubeID(html) : null)
 
-          plyr.on('error', onError)
+  if (!videoSrc) {
+    return null
+  }
 
-          window.addEventListener('error', onError)
-          window.addEventListener('unhandledrejection', onError)
-
-          return () => {
-            window.removeEventListener('error', onError)
-            window.removeEventListener('unhandledrejection', onError)
-
-            plyr.off('error', onError)
-          }
-        }
-      }
-    } catch (err) {
-      console.error(err)
-      containerRef.current.innerHTML = `<div class="Video-inner">${html}</div>`
-      setHasError(true)
-    }
-  }, [plyrRef?.current?.plyr, html, containerRef])
-
-  const MemoVideo = React.useMemo(() => {
-    if (!provider_name || !video_id) return null
-
-    return (
-      <Plyr
-        ref={plyrRef}
-        id={video_id}
-        source={{
-          type: 'video',
-          sources: [
-            {
-              src:
-                video_id ??
-                (provider_name === 'YouTube' && html
-                  ? getYoutubeID(html)
-                  : false),
-              provider: provider_name.toLowerCase(),
-            },
-          ],
-        }}
-        options={{
-          fullscreen: {
-            enabled: true,
-            fallback: true,
-            iosNative: 'force',
-            container: null,
-          },
-          controls: [
-            'play-large',
-            'play',
-            'progress',
-            'duration',
-            'mute',
-            'volume',
-            'fullscreen',
-          ],
-          youtube: { noCookie: true },
-        }}
-      />
-    )
-  }, [video_id, provider_name, html])
+  const src =
+    provider === 'youtube'
+      ? `https://www.youtube.com/embed/${videoSrc}`
+      : `https://player.vimeo.com/video/${videoSrc}`
 
   return (
     <div
-      ref={containerRef}
+      ref={ref}
       className={classNames('Video', {
-        [`is-${provider_name.toLowerCase()}`]: provider_name,
+        [`is-${provider}`]: provider,
         'is-inView': inView,
-        'is-error': hasError,
       })}
       style={{
         '--video-width': `${width}`,
         '--video-height': `${height}`,
       }}
     >
-      <div ref={ref} className="Video-inner">
-        {MemoVideo}
+      <div className="Video-inner">
+        <iframe
+          src={src}
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
       </div>
     </div>
   )
